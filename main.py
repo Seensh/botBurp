17869ff8069ad1d2b5d650efda5c48958878c91c
Felipe <felipe.barbosa001@hotmail.com>
2022-09-01 23:54:33 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyautogui
+import keyboard
+import time
+
+def functionMain():
+        count = 1
+        while keyboard.is_pressed('c') == False:
+                #count = file.read()
+                count = count + 1
+                print(count)
+                time.sleep(2)
+                #CAPTURA POSIÇÃO
+                positionSend = pyautogui.locateOnScreen('send.png', grayscale=True,confidence=0.8)
+                positionCenter = pyautogui.center(positionSend)
+                pyautogui.click(positionCenter.x,positionCenter.y)
+
+functionMain()
